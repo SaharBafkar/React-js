f2ddc1954731f8c0500fe15cc76b0cda6009aa93
Sahar Bafkar <104864189+SaharBafkar@users.noreply.github.com>
2022-11-30 15:12:52 +0330

    Add files via upload

---
+++ CounterApp/counter.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: ["tag1", "tag2", "tag3"],
+  // };
+  styles = {
+    fontSize: 15,
+    fontWeight: "bolder",
+  };
+
+   render() {
+    return (
+      // <React.Fragment>
+
+      // </React.Fragment>
+      <div>
+        <h4>{this.props.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={()=>this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
+    // return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
